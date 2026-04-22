48d4429054de2a61c72afcaf99845dbc3cbb7e59
jimmy <jimoseipeace2bb@gmail.com>
2026-04-21 21:38:38 -0500

    implemented the maxDepth of BST

--- trees/binary_search_tree.py
+++ trees/binary_search_tree.py
@@ -1,1 +1,79 @@
 """Binary search tree implementation."""
+from node import TreeNode
+
+
+class BinarySearchTree:
+    def __init__(self,root:TreeNode| None = None) -> None:
+        self.root = root
+
+    def insert(self,value:int) -> None:
+        current = self.root
+        if not current:
+            current = TreeNode(value)
+            self.root = current
+            return
+
+        while current:
+            if value < current.value:
+                if current.left is None:
+                    current.left = TreeNode(value)
+                    return
+                current = current.left
+            elif value > current.value:
+                if current.right is None:
+                    current.right = TreeNode(value)
+                    return
+                current = current.right
+            else:
+                return
+
+    def search(self, value: int):
+        current = self.root
+
+        if not current: return None
+
+        while current:
+            if value < current.value:
+                current = current.left
+            elif value > current.value:
+                current = current.right
+            else:
+                return current.value
+
+        return None
+
+    def findMin(self):
+        current = self.root
+
+        if not current: return None
+
+        while current.left:
+            current = current.left
+        return current.value
+
+    def findMax(self):
+        current = self.root
+        if not current: return None
+
+        while current.right:
+            current = current.right
+        return current.value
+
+    def maxDepth(self,root):
+        if not root: return 0
+
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+
+        return max(left_depth, right_depth) + 1
+
+
+
+
+
+
+
+
+
+
+
--- trees/main.py
+++ trees/main.py
@@ -1,6 +1,7 @@
 from node import TreeNode
 from traversals import (inorder_traversal,
-                        preorder_traversal, post_order_traversal, level_order_traversal)
+                        preorder_traversal, post_order_traversal)
+from binary_search_tree import BinarySearchTree
 
 def build_sample_tree() -> TreeNode:
     root = TreeNode(10)
@@ -16,8 +17,17 @@ def build_sample_tree() -> TreeNode:
 
 
 
-
-
 if __name__ == "__main__":
-    root = build_sample_tree()
-    level_order_traversal(root)
+    bst = BinarySearchTree()
+    bst.insert(10)
+    bst.insert(5)
+    bst.insert(15)
+    bst.insert(3)
+    bst.insert(7)
+    bst.insert(12)
+    bst.insert(18)
+
+    # print(bst.search(20))
+    print(bst.maxDepth(bst.root))
+
+    # inorder_traversal(bst.root)
